68e430f20961aa38ea19e5932d31c5869fe61dd8
Marcy Petroccione <marcypetro@github.com>
2014-04-06 12:49:00 -0400

    Add styling, add meta tag editor plugin

--- wp-content/themes/folder/page.php
+++ wp-content/themes/folder/page.php
@@ -7,8 +7,9 @@
 
 <?php get_header() ?>
 	<?php the_post() ?>
+	<?php $page_id = in_array($id, array(7, 9)) ? 'posts-list' : 'page-content'; ?>
 	<!-- page content-->
-	<div id="page-content" class="cf">        	
+	<div id="<?php echo $page_id ?>" class="cf">        	
     		
     	<!-- entry-content -->	
     	<div class="entry-content cf">
@@ -17,4 +18,9 @@
 		</div>
 	</div>
 	<!-- ENDS page-content -->	
+	<?php if (in_array($id, array(7, 9))) : ?>
+		<!-- Sidebar for Who We Are and What We Do pages -->
+		<?php  get_sidebar() ?>	
+	<?php endif; ?>
+	
 <?php get_footer() ?>
